12fb5cc5896d7229e061d67d8d7ffc028af721a5
antikorps <antikorps@gmail.com>
2024-08-12 13:40:32 +0200

    fronted improvements: learning use Astro components

---
+++ frontend/src/types/index.d.ts
@@ -0,0 +1,25 @@
+
+export interface LanguageTemplate {
+    lang: string;
+    labelLanguage: string;
+    fileFormLabel: string;
+    fileFormExtensionValid: string;
+    fileResetButton: string;
+    fileUploadButton: string;
+    errorTextHeading: string;
+    resultsNumber: string;
+    orderLabel: string;
+    orderSelectOption1: string;
+    orderSelectOption2: string;
+    orderSelectOption3: string;
+    orderSelectOption4: string;
+    exportLabel: string;
+    useSummary: string;
+    useExplication: string;
+    useResults: string;
+    useGreen: string;
+    useYellow: string;
+    userRed: string;
+    useInfoStatusCode: string;
+    useVisitGitHub: string;
+}
