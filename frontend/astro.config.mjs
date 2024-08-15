a384d3a16bb08f2ce831b52df5458a8765fee558
Marc Perez de Tudela <mperezdetudelaaznar@gmail.com>
2024-08-16 00:15:46 +0200

    frontend: refactor navbar

--- frontend/astro.config.mjs
+++ frontend/astro.config.mjs
@@ -1,14 +1,15 @@
 import { defineConfig } from "astro/config";
-
 import tailwind from "@astrojs/tailwind";
 import viteConfig from "./vite.config.js";
 
+import lit from "@astrojs/lit";
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
+  integrations: [tailwind(), lit()],
   i18n: {
     defaultLocale: "es",
-    locales: ["es", "en", "fr"],
+    locales: ["es", "en", "fr"]
   },
-  vite: viteConfig,
+  vite: viteConfig
 });
