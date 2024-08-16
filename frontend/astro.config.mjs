8d075f3586e95fbb92032f5d4969b5cbe206cb0c
Marc Perez de Tudela <mperezdetudelaaznar@gmail.com>
2024-08-16 12:12:28 +0200

    frontend: change lit for react

--- frontend/astro.config.mjs
+++ frontend/astro.config.mjs
@@ -2,14 +2,14 @@ import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import viteConfig from "./vite.config.js";
 
-import lit from "@astrojs/lit";
+import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), lit()],
+  integrations: [tailwind(), react()],
   i18n: {
     defaultLocale: "es",
-    locales: ["es", "en", "fr"],
+    locales: ["es", "en", "fr"]
   },
-  vite: viteConfig,
+  vite: viteConfig
 });
