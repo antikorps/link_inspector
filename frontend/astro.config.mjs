2e73b28206f2cb3acc9d051ac86448cb78d72153
Marc Perez de Tudela <mperezdetudelaaznar@gmail.com>
2024-08-18 06:18:47 +0200

    frontend: add seo and performance optimizations

--- frontend/astro.config.mjs
+++ frontend/astro.config.mjs
@@ -1,12 +1,13 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import viteConfig from "./vite.config.js";
-
 import react from "@astrojs/react";
+
+import compressor from "astro-compressor";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), react()],
+  integrations: [tailwind(), react(), compressor({ gzip: true, brotli: false })],
   i18n: {
     defaultLocale: "es",
     locales: ["es", "en", "fr"]
