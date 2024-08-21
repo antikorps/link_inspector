39edf0169ff7f1843dc2cfa60fb10f57a58abda1
Marc Perez de Tudela <mperezdetudelaaznar@gmail.com>
2024-08-21 23:07:46 +0200

    frontend: add alpine integration

--- frontend/astro.config.mjs
+++ frontend/astro.config.mjs
@@ -2,12 +2,16 @@ import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import viteConfig from "./vite.config.js";
 import react from "@astrojs/react";
-
 import compressor from "astro-compressor";
+
+import alpinejs from "@astrojs/alpinejs";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), react(), compressor({ gzip: true, brotli: false })],
+  integrations: [tailwind(), react(), compressor({
+    gzip: true,
+    brotli: false
+  }), alpinejs()],
   i18n: {
     defaultLocale: "es",
     locales: ["es", "en", "fr"]
