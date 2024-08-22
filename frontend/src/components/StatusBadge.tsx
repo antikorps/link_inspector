121ec5aa6762f901f04eff4750e08ad77a95aca5
Marc Perez de Tudela <mperezdetudelaaznar@gmail.com>
2024-08-22 23:45:37 +0200

    frontend: add min width for statusBadge

--- frontend/src/components/StatusBadge.tsx
+++ frontend/src/components/StatusBadge.tsx
@@ -20,26 +20,26 @@ export const StatusBadge = ({ status, active }: Props) => {
       )}
 
       {active == 0 && (
-        <span className="items-center bg-gray-100 text-gray-800 text-xs font-medium px-2.5 py-0.5 rounded-full dark:bg-gray-300 dark:text-gray-900 hidden md:inline-flex">
+        <span className="min-w-14 min-h-4 items-center bg-gray-100 text-gray-800 text-xs font-medium px-2.5 py-0.5 rounded-full dark:bg-gray-300 dark:text-gray-900 hidden md:inline-flex">
           <span className="w-2 h-2 me-1 bg-gray-500 rounded-full"></span>
           ?
         </span>
       )}
 
       {active == 1 && (
-        <span className="items-center bg-green-100 text-green-800 text-xs font-medium px-2.5 py-0.5 rounded-full dark:bg-green-900 dark:text-green-300 hidden md:inline-flex">
+        <span className="min-w-14 min-h-4 items-center bg-green-100 text-green-800 text-xs font-medium px-2.5 py-0.5 rounded-full dark:bg-green-900 dark:text-green-300 hidden md:inline-flex">
           <span className="w-2 h-2 me-1 bg-green-500 rounded-full"></span>
           {status}
         </span>
       )}
       {active == 2 && (
-        <span className="items-center bg-yellow-100 text-yellow-800 text-xs font-medium px-2.5 py-0.5 rounded-full dark:bg-yellow-900 dark:text-yellow-300 hidden md:inline-flex">
+        <span className="min-w-14 min-h-4 items-center bg-yellow-100 text-yellow-800 text-xs font-medium px-2.5 py-0.5 rounded-full dark:bg-yellow-900 dark:text-yellow-300 hidden md:inline-flex">
           <span className="w-2 h-2 me-1 bg-yellow-500 rounded-full"></span>
           {status}
         </span>
       )}
       {active == 3 && (
-        <span className="items-center bg-red-100 text-red-800 text-xs font-medium px-2.5 py-0.5 rounded-full dark:bg-red-900 dark:text-red-300 hidden md:inline-flex">
+        <span className="min-w-14 min-h-4 items-center bg-red-100 text-red-800 text-xs font-medium px-2.5 py-0.5 rounded-full dark:bg-red-900 dark:text-red-300 hidden md:inline-flex">
           <span className="w-2 h-2 me-1 bg-red-500 rounded-full"></span>
           {status ? status : "ERR"}
         </span>
